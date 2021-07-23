5ccf3d02fb92bfbd74bd1826da86c42bcfdd823c
SuchitaSriramka <suchitasriramka@gmail.com>
2021-07-23 14:55:18 +0530

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -33,3 +33,48 @@ public class MergeSort{
             System.out.print(i+" ");
     }
 }
+/*
+import java.util.*;
+public class Main{
+    public static int merge(int arr[],int leftStart,int leftEnd,int rightStart,int rightEnd){
+        int temp[] = new int[rightEnd-leftStart+1];
+        int inversionCount = 0;
+        int position = 0;
+        int start = leftStart;
+        int start2 = rightStart;
+        while(leftStart<=leftEnd && rightStart<=rightEnd){
+            if(arr[leftStart]<=arr[rightStart])
+                temp[position++] = arr[leftStart++];
+            else{
+                temp[position++] = arr[rightStart++];
+                inversionCount+=start2-leftStart;
+            }
+        }
+        while(leftStart<=leftEnd)
+            temp[position++] = arr[leftStart++];
+        while(rightStart<=leftStart)
+            temp[position++] = arr[rightStart++];
+        for(int i=0;i<position;i++)
+            arr[i+start] = temp[i];
+        return inversionCount;
+    }
+    public static int mergeSort(int arr[],int low,int high){
+        int count = 0;
+        if(low < high){
+        int mid = (high+low)/2;
+        count+=mergeSort(arr, low, mid);
+        count+=mergeSort(arr, mid+1, high);
+        count+=merge(arr, low, mid, mid+1, high);            
+        }
+        return count;
+    }
+    public static void main (String[] args) {
+        int[] arr = {1, 20, 6, 4, 5};
+        int n = arr.length;
+        int count = mergeSort(arr,0,arr.length-1);
+        for(int i:arr)
+            System.out.print(i+" ");
+        System.out.println(count);
+    }
+}
+*/
